a084abbb2cdd3a8196e1580d128ba5b9adea7d60
Reinhard Weiss <reinhard.weiss@bnv-bamberg.de>
2022-12-31 22:45:20 +0100

    Removed memory-leak eating all the memory at least on linux mint when another audit client is present on (late) startup

--- src/listener.cc
+++ src/listener.cc
@@ -789,9 +789,8 @@ void AuditListener::exec()
             default:
                 break;
         }
-        auparse_reset(au);
+        auparse_destroy(au);
     }
-    auparse_destroy(au);
 }
 
 
